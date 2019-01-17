85244cb7b5e469d8439446ab5f0efdd104808360
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-17 21:01:27 +0530

    Update DataMatchUploadRequest.cs

--- MCAPI/Models/DataMatchUploadRequest.cs
+++ MCAPI/Models/DataMatchUploadRequest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +11,6 @@ namespace Mc.TD.Upload.Domain.DataMatch
     {
         public Header RequestHeader { get; set; }
 
-        public IList<Detail> RequestDetails { get; set; }
+        public IList<Detail> RequestDetail { get; set; }
     }
 }
